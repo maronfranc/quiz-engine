7d9f04e0d2599b53e9393c04fd4cd851f53bca21
Máron França <maron.franca@gmail.com>
2024-08-03 16:46:15 -0300

    [css] add base grid.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,30 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      Home
+    <div className="grid">
+      <header>
+        Header
+      </header>
+
+      <main>
+        main
+      </main>
+
+      <aside>
+        <div>
+          A
+        </div>
+        <div>
+          B
+        </div>
+      </aside>
+
+      <footer>
+        <button>Previous</button>
+        <button>Next</button>
+      </footer>
     </div>
-  )
+  );
 }
 
 export default App
